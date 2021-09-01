db97eddf826d82406022dc122a4036b5a6e6be3e
Yu Da Lau <yuda@trypencil.com>
2021-09-01 10:50:39 +0800

    comment1

---
+++ html.js
@@ -0,0 +1,17 @@
+// Sample server that serves direct HTML
+// comment 1
+const http = require("http");
+
+const host = '0.0.0.0';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(`<html><body><h1>This is HTML</h1></body></html>`);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
